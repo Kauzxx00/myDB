276df9e4712c43d317d47327cfff69ad1faefd80
Kauzxx00 <ed3ard0k13@gmail.com>
2025-01-08 20:38:54 -0300

    feat: Mudança no separador para sub chavez e adição de parâmetro para encontrar subchaves.

--- functions/database/get.js
+++ functions/database/get.js
@@ -1,15 +1,29 @@
 const loadData = require("../loadData.js");
 
-const get = (fileName, id) => {
+const get = (fileName, id, keys) => {
   if (!id) throw new Error('O identificador é obrigatório para a função "get".');
-  
+
   const database = loadData(fileName);
 
   if (!database[id]) {
     throw new Error(`Nenhum item encontrado com o identificador "${id}".`);
   }
 
-  return database[id];
+  const data = database[id];
+
+  if (!keys) return data;
+
+  const keysArray = keys.split(".");
+  let result = data;
+
+  for (const key of keysArray) {
+    if (result[key] === undefined) {
+      throw new Error(`Propriedade "${key}" não encontrada em "${keys}".`);
+    }
+    result = result[key];
+  }
+
+  return result;
 };
 
 module.exports = get;
