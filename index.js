6862a1495a4bdccf5eb85001afe18975ca027415
Kauzxx00 <ed3ard0k13@gmail.com>
2025-01-08 20:59:22 -0300

    feat(functions): Adicionado parâmetro 'key' à função get.

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ class MyDB {
     if (!fileName) throw new Error("O nome do arquivo é obrigatório.");
     this.fileName = fileName.endsWith(".json") ? fileName : `${fileName}.json`;
 
-    this.create = (key, data) => create(this.fileName, key, data);
-    this.update = (key, updates) => update(this.fileName, key, updates);
-    this.get = (key) => get(this.fileName, key);
-    this.delete = (key) => del(this.fileName, key);
-    this.push = (key, value) => push(this.fileName, key, value);
+    this.create = (id, data) => create(this.fileName, id, data);
+    this.update = (id, updates) => update(this.fileName, id, updates);
+    this.get = (id, keys) => get(this.fileName, id, keys);
+    this.delete = (id) => del(this.fileName, id);
+    this.push = (id, value) => push(this.fileName, id, value);
   }
 }
 
