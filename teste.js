d48ee66c2cb47bf1f0c1b42b2334bd69c3ed6e58
Kauzxx00 <ed3ard0k13@gmail.com>
2025-01-08 21:03:23 -0300

    feat: alterações simples.

--- teste.js
+++ teste.js
@@ -2,3 +2,14 @@ const { MyDB } = require("./index.js");
 const db = new MyDB();
 
 db.create("12345");
+db.update("12345", { "oi.opa": "eae" });
+db.push("12345", { "oi.array": "bla"});
+
+console.log(
+	db.get("12345", "oi.opa") // retorna: eae
+);
+
+console.log(
+	db.get("12345").oi
+	// retorna: { opa: 'eae', array: [ 'bla' ] }
+);
