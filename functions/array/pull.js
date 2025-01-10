a5c425be0d3f1d9c08967f56e39950a7cedecd77
Kauzxx00 <ed3ard0k13@gmail.com>
2025-01-10 14:03:19 -0300

    feat: Adicionada função 'pull'

---
+++ functions/array/pull.js
@@ -0,0 +1,51 @@
+const loadData = require("../loadData.js");
+const saveData = require("../saveData.js");
+
+function getNestedArray(obj, path) {
+  const keys = path.split(".");
+  let current = obj;
+
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+
+    if (i === keys.length - 1) {
+      if (!Array.isArray(current[key])) {
+        throw new Error(`"${path}" não é um array ou não existe.`);
+      }
+
+      return current[key];
+    }
+
+    if (!current[key]) {
+      throw new Error(`Caminho "${path}" não encontrado.`);
+    }
+
+    current = current[key];
+  }
+}
+
+function pull(fileName, id, values) {
+  if (!id || typeof values !== "object" || Array.isArray(values)) {
+    throw new Error("ID e objeto de atualização são obrigatórios.");
+  }
+
+  const database = loadData(fileName);
+  if (!database[id]) {
+    throw new Error(`Nenhum item encontrado com o ID "${id}".`);
+  }
+
+  for (let path in values) {
+    const array = getNestedArray(database[id], path);
+
+    const valueToRemove = values[path];
+    const index = array.indexOf(valueToRemove);
+    if (index === -1) {
+      throw new Error(`Valor "${valueToRemove}" não encontrado no array "${path}".`);
+    }
+    array.splice(index, 1);
+  }
+
+  saveData(fileName, database);
+}
+
+module.exports = pull;
