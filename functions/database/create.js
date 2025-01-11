e3593fed66eb9bcc39bac88fa4c22760dece1e40
Kauzxx00 <ed3ard0k13@gmail.com>
2025-01-11 14:31:14 -0300

    feat: Agora é possivel criar com sub objetos

--- functions/database/create.js
+++ functions/database/create.js
@@ -1,6 +1,22 @@
 const loadData = require("../loadData.js");
 const saveData = require("../saveData.js");
 
+// Função para adicionar valores em objetos aninhados
+const setNestedValue = (obj, path, value) => {
+  const keys = path.split(".");
+  let current = obj;
+
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    if (i === keys.length - 1) {
+      current[key] = value;
+    } else {
+      if (!current[key] || typeof current[key] !== "object") current[key] = {};
+      current = current[key];
+    }
+  }
+};
+
 const create = (fileName, id, data = {}) => {
   if (!id || typeof data !== "object" || Array.isArray(data)) {
     throw new Error('Um identificador e um objeto válido são obrigatórios para a função "create".');
@@ -12,7 +28,11 @@ const create = (fileName, id, data = {}) => {
     throw new Error(`Item com o identificador "${id}" já existe!`);
   }
 
-  database[id] = data;
+  database[id] = {};
+  for (const path in data) {
+    setNestedValue(database[id], path, data[path]);
+  }
+
   saveData(fileName, database);
 };
 
